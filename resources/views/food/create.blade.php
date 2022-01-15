7ded8e9d6ce8cef7e408346adbeb919cb89f6774
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2022-01-15 16:27:20 +0700

    Update view port

--- resources/views/food/create.blade.php
+++ resources/views/food/create.blade.php
@@ -1,109 +1,114 @@
 @extends('layouts.menu')
 @section('page-content')
-<section class="vh-100 gradient-custom">
-    <div class=" py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card bg-dark text-white" style="border-radius: 1rem;">
-            <div class="card-body p-5 text-center">
-  
-              <div class="mb-md-5 mt-md-4 ">
-  
-                <h2 class="fw-bold mb-2 text-uppercase">Create Food</h2>
-                
-                <form action="/createFood" method="post" enctype="multipart/form-data">
-                @csrf
+    <section class="gradient-custom">
+        <div class=" py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
 
-                <div class="form-outline form-white mb-4 mt-5">
-                  <input type="text" id="typeEmailX" name="name" class="form-control form-control-lg" />
-                  <label class="form-label" for="typeEmailX">Name</label>
+                            <div class="mb-md-5 mt-md-4 ">
+
+                                <h2 class="fw-bold mb-2 text-uppercase">Create Food</h2>
+
+                                <form action="/createFood" method="post" enctype="multipart/form-data">
+                                    @csrf
+
+                                    <div class="form-outline form-white mb-4 mt-5">
+                                        <input type="text" id="typeEmailX" name="name"
+                                            class="form-control form-control-lg" />
+                                        <label class="form-label" for="typeEmailX">Name</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4 mt-5">
+                                        <div class="field_wrapper">
+                                            <a href="javascript:void(0);" class="add_button btn btn-success"
+                                                title="Add field">Add</a>
+
+                                            <select class="form-select form-control form-control-lg"
+                                                aria-label="Default select example" name="field_name[]">
+                                                <option selected>Choose Ingredient</option>
+                                                @foreach ($ingredients as $ingredient)
+                                                    <option value="{{ $ingredient->id }}">
+                                                        {{ $ingredient->ingredient_name }}</option>
+                                                @endforeach
+                                            </select>
+                                            <br>
+
+                                        </div>
+                                    </div>
+
+                                    <label class="form-label" for="typeEmailX">Ingredient</label>
+
+
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="text" name="calories" class="form-control form-control-lg" />
+                                        <label class="form-label" for="name">Calories</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="text" name="rating" class="form-control form-control-lg" />
+                                        <label class="form-label" for="name">Rating</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="text" name="duration" class="form-control form-control-lg" />
+                                        <label class="form-label" for="name">Duration</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="file" name="image" class="form-control form-control-lg" />
+                                        <label class="form-label" for="name">Image</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <textarea class="form-control form-control-lg" name="description" id="" cols="30"
+                                            rows="10"></textarea>
+                                        <label class="form-label" for="name">Description</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <textarea class="form-control form-control-lg" name="procedure" id="" cols="30"
+                                            rows="10"></textarea>
+                                        <label class="form-label" for="name">Procedure</label>
+                                    </div>
+
+                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Add</button>
+
+                                </form>
+
+                            </div>
+
+                            <div>
+                                <a href="/food" class="text-white-50 fw-bold">Back</a></p>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
 
-                <div class="form-outline form-white mb-4 mt-5">
-                  <div class="field_wrapper">
-                    <a href="javascript:void(0);" class="add_button btn btn-success" title="Add field">Add</a>
+                <script type="text/javascript">
+                    $(document).ready(function() {
+                        var maxField = 20;
+                        var addButton = $('.add_button');
+                        var wrapper = $('.field_wrapper');
+                        var fieldHTML =
+                            '<div class="mt-3"><select class="form-select form-control form-control-lg" aria-label="Default select example" name="field_name[]">  <option selected>Choose Ingredient</option>@foreach ($ingredients as $ingredient)<option value="{{ $ingredient->id }}">{{ $ingredient->ingredient_name }}</option>@endforeach</select></div>';
+                        var x = 1;
 
-                    <select class="form-select form-control form-control-lg" aria-label="Default select example" name="field_name[]">
-                      <option selected>Choose Ingredient</option>
-                      @foreach ($ingredients as $ingredient)
-                        <option value="{{ $ingredient->id}}">{{ $ingredient->ingredient_name}}</option>
-                      @endforeach
-                    </select>
-                    <br>
+                        $(addButton).click(function() {
+                            if (x < maxField) {
+                                x++;
+                                $(wrapper).append(fieldHTML);
+                            }
+                        });
 
-                  </div>
-                </div>
+                        $(wrapper).on('click', '.remove_button', function(e) {
+                            e.preventDefault();
+                            $(this).parent('div').remove();
+                            x--;
+                        });
+                    });
+                </script>
 
-                <label class="form-label" for="typeEmailX">Ingredient</label>
-
-
-                <div class="form-outline form-white mb-4">
-                    <input type="text"  name="calories" class="form-control form-control-lg" />
-                    <label class="form-label" for="name">Calories</label>
-                </div>
-  
-                <div class="form-outline form-white mb-4">
-                    <input type="text"  name="rating" class="form-control form-control-lg" />
-                    <label class="form-label" for="name">Rating</label>
-                </div>
-
-                <div class="form-outline form-white mb-4">
-                    <input type="text"  name="duration" class="form-control form-control-lg" />
-                    <label class="form-label" for="name">Duration</label>
-                </div>
-
-                <div class="form-outline form-white mb-4">
-                  <input type="file"  name="image" class="form-control form-control-lg" />
-                  <label class="form-label" for="name">Image</label>
-              </div>
-
-                <div class="form-outline form-white mb-4">
-                    <textarea  class="form-control form-control-lg" name="description" id="" cols="30" rows="10"></textarea>
-                    <label class="form-label" for="name">Description</label>
-                </div>
-
-                <div class="form-outline form-white mb-4">
-                    <textarea  class="form-control form-control-lg" name="procedure" id="" cols="30" rows="10"></textarea>
-                    <label class="form-label" for="name">Procedure</label>
-                </div>
-  
-                <button class="btn btn-outline-light btn-lg px-5" type="submit">Add</button>
-  
-                </form>
-
-              </div>
-  
-              <div>
-                <a href="/food" class="text-white-50 fw-bold">Back</a></p>
-              </div>
-  
-            </div>
-          </div>
-</div>
-
-<script type="text/javascript">
-  $(document).ready(function () {
-      var maxField = 20;
-      var addButton = $('.add_button');
-      var wrapper = $('.field_wrapper');
-      var fieldHTML =
-          '<div class="mt-3"><select class="form-select form-control form-control-lg" aria-label="Default select example" name="field_name[]">  <option selected>Choose Ingredient</option>@foreach ($ingredients as $ingredient)<option value="{{ $ingredient->id}}">{{ $ingredient->ingredient_name}}</option>@endforeach</select></div>';
-      var x = 1;
-
-      $(addButton).click(function () {
-          if (x < maxField) {
-              x++;
-              $(wrapper).append(fieldHTML);
-          }
-      });
-
-      $(wrapper).on('click', '.remove_button', function (e) {
-          e.preventDefault();
-          $(this).parent('div').remove();
-          x--;
-      });
-  });
-
-</script>
-
-@endsection
+            @endsection
--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -1,62 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 
     <title>Login</title>
-  </head>
+</head>
 
-  <style>
-      .gradient-custom{
+<style>
+    .gradient-custom {
         background: linear-gradient(to right, rgb(250, 123, 4), rgb(233, 177, 73))
-      }
-  </style>
+    }
 
-  <body>
-    <section class="vh-100 gradient-custom">
+</style>
+
+<body>
+    <section class="gradient-custom">
         <div class="container py-5 h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                <div class="card-body p-5 text-center">
-      
-                  <div class="mb-md-5 mt-md-4 ">
-      
-                    <h2 class="fw-bold mb-2 text-uppercase">Register</h2>
-                    
-                    <form action="/register" method="post" enctype="multipart/form-data">
-                    @csrf
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
 
-                    <div class="form-outline form-white mb-4 mt-5">
-                      <input type="email" id="typeEmailX" name="email" class="form-control form-control-lg" />
-                      <label class="form-label" for="typeEmailX">Email</label>
-                    </div>
-                    <div class="form-outline form-white mb-4">
-                        <input type="text"  name="name" class="form-control form-control-lg" />
-                        <label class="form-label" for="name">Name</label>
-                      </div>
-      
-                    <div class="form-outline form-white mb-4">
-                      <input type="password" id="typePasswordX" name="password" class="form-control form-control-lg" />
-                      <label class="form-label" for="typePasswordX">Password</label>
-                    </div>
-      
-                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Register</button>
-      
-                    </form>
+                            <div class="mb-md-5 mt-md-4 ">
 
-                  </div>
-      
-                  <div>
-                    <p class="mb-0">Have an account? <a href="/login" class="text-white-50 fw-bold">Login</a></p>
-                  </div>
-      
+                                <h2 class="fw-bold mb-2 text-uppercase">Register</h2>
+
+                                <form action="/register" method="post" enctype="multipart/form-data">
+                                    @csrf
+
+                                    <div class="form-outline form-white mb-4 mt-5">
+                                        <input type="email" id="typeEmailX" name="email"
+                                            class="form-control form-control-lg" />
+                                        <label class="form-label" for="typeEmailX">Email</label>
+                                    </div>
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="text" name="name" class="form-control form-control-lg" />
+                                        <label class="form-label" for="name">Name</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="password" id="typePasswordX" name="password"
+                                            class="form-control form-control-lg" />
+                                        <label class="form-label" for="typePasswordX">Password</label>
+                                    </div>
+
+                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Register</button>
+
+                                </form>
+
+                            </div>
+
+                            <div>
+                                <p class="mb-0">Have an account? <a href="/login"
+                                        class="text-white-50 fw-bold">Login</a></p>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
-              </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                                crossorigin="anonymous"></script>
+</body>
+
 </html>
